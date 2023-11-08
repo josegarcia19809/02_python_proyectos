a18218e5da5d45fb3c47c02c8f3c10e9f2adfd1d
Jose Garcia <josegarcia19809@gmail.com>
2023-11-08 16:13:17 -0600

    Creación de listas

---
+++ crear_listas.py
@@ -0,0 +1,18 @@
+"""
+Primero, define una lista llamada my_stuff. Debe tener al menos 4 elementos.
+Los datos dependen completamente de ti, pero debe contener al menos 1 cadena y 1
+flotante.
+"""
+
+# Define my_stuff
+my_stuff = ["hola", 3.5, 4, "true"]
+print(my_stuff)
+
+"""
+A continuación, define una lista llamada nums . Debe ser una lista que contenga todos 
+los números entre 1 y 99 (incluido el 99). ¡No escribas esto manualmente! 
+"""
+
+# Define nums
+nums = list(range(1, 100))
+print(nums)
