05093708be49c61f5dbd321ee573aa026fbffc43
Jose Garcia <josegarcia19809@gmail.com>
2023-11-26 18:30:40 -0600

    Manejo de cadenas

---
+++ cadenas/caracteres_de_cadena.py
@@ -0,0 +1,7 @@
+# Escribir por pantalla cada carácter de una cadena introducida por teclado.
+
+mensaje = input("Introduce un texto: ")
+for caracter in mensaje:
+    print(caracter)
+
+print("-"*100)
