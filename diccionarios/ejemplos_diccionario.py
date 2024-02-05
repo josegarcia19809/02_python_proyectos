5a5387d63f2981c28d5e4cec4d2b5548feab0c46
josegarcia19809 <josegarcia19809@gmail.com>
2024-02-05 13:11:42 -0600

    Modificando diccionario

--- diccionarios/ejemplos_diccionario.py
+++ diccionarios/ejemplos_diccionario.py
@@ -36,3 +36,15 @@ for item in pol_esp_dictionary:
 #         woda
 #         gleba
 
+
+print()
+# Escribe un programa que "una" los dos diccionarios (d1 y d2) para crear uno nuevo (d3).
+
+d1 = {'Adam Smith': 'A', 'Judy Paxton': 'B+'}
+d2 = {'Mary Louis': 'A', 'Patrick White': 'C'}
+d3 = {}
+
+for item in (d1, d2):
+    d3.update(item)
+
+print(d3)
