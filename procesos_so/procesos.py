dfd0ce40b0eaf1e7db46e8f11a8aff7d3cf3478f
josegarcia19809 <josegarcia19809@gmail.com>
2024-03-14 07:58:25 -0600

    Menú de programa de procesos

--- procesos_so/procesos.py
+++ procesos_so/procesos.py
@@ -1,7 +1,16 @@
-import os
-os.system("cls")
-
+procesos = []
 while True:
-    proceso = input("Ingresa un proceso: ")
-    print(proceso)
-    print("Siguiente...")
+    accion_usuario = input("Escribe agregar, mostrar o salir: ")
+    accion_usuario = accion_usuario.strip()
+    if accion_usuario == "agregar":
+        nuevo_proceso = input("Ingresa un proceso: ")
+        procesos.append(nuevo_proceso.capitalize())
+    elif accion_usuario == "mostrar":
+        for proceso in procesos:
+            print(proceso)
+    elif accion_usuario == "salir":
+        break
+    else:
+        print("Hey, has ingresado una instrucción desconocida")
+
+print("Adios")
