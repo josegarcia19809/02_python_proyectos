84a530bd33a4166b6e38e8697c4b69c96bd5e75f
josegarcia19809 <josegarcia19809@gmail.com>
2024-04-09 21:16:06 -0600

    Storing User Input #lists

---
+++ lista-tareas/app1.py
@@ -0,0 +1,9 @@
+tarea1 = input("Ingresa una tarea: ")
+tarea2 = input("Ingresa una tarea: ")
+tarea3 = input("Ingresa una tarea: ")
+
+tareas = [tarea1, tarea2, tarea3]
+print(tareas)
+
+print(type(tarea1))
+print(type(tareas))
