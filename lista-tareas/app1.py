59869a9e11a2d4ca714f7ddc7e7357aea96296aa
josegarcia19809 <josegarcia19809@gmail.com>
2024-04-09 21:18:59 -0600

    Getting User Input Repeatedly #while-loop

--- lista-tareas/app1.py
+++ lista-tareas/app1.py
@@ -1,9 +1,4 @@
-tarea1 = input("Ingresa una tarea: ")
-tarea2 = input("Ingresa una tarea: ")
-tarea3 = input("Ingresa una tarea: ")
-
-tareas = [tarea1, tarea2, tarea3]
-print(tareas)
-
-print(type(tarea1))
-print(type(tareas))
+while True:
+    nueva_tarea = input("Ingresa una tarea: ")
+    print(nueva_tarea)
+    print("Siguiente...")
