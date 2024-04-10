919148ee8cc01b00061969b66b079fdbbe15faac
josegarcia19809 <josegarcia19809@gmail.com>
2024-04-09 21:28:24 -0600

    Improving the Program Output #for-loops

--- lista-tareas/app1.py
+++ lista-tareas/app1.py
@@ -1,4 +1,16 @@
+tareas = []
 while True:
-    nueva_tarea = input("Ingresa una tarea: ")
-    print(nueva_tarea)
-    print("Siguiente...")
+    accion_usuario = input("Escribe agregar, mostrar o salir: ")
+    accion_usuario = accion_usuario.strip()
+    if accion_usuario == "agregar":
+        nueva_tarea = input("Ingresa una tarea: ")
+        tareas.append(nueva_tarea.capitalize())
+    elif accion_usuario == "mostrar":
+        for tarea in tareas:
+            print(tarea)
+    elif accion_usuario == "salir":
+        break
+    else:
+        print("Hey, ingresa una opcion valida")
+
+print("¡Adios!")
