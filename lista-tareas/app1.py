762e59067545decb66831cc2d28b540f004aef02
josegarcia19809 <josegarcia19809@gmail.com>
2024-04-13 20:15:42 -0600

    Editar y eliminar tareas

--- lista-tareas/app1.py
+++ lista-tareas/app1.py
@@ -1,16 +1,33 @@
-tareas = []
+tareas = ["Barrer", "Trapear", "Planchar"]
+
 while True:
-    accion_usuario = input("Escribe agregar, mostrar o salir: ")
+    accion_usuario = input("Escribe agregar, mostrar, editar, quitar o salir: ")
     accion_usuario = accion_usuario.strip()
+
     if accion_usuario == "agregar":
-        nueva_tarea = input("Ingresa una tarea: ")
-        tareas.append(nueva_tarea.capitalize())
+        tarea = input("Ingresa una tarea: ")
+        tareas.append(tarea.capitalize())
+
     elif accion_usuario == "mostrar":
-        for tarea in tareas:
-            print(tarea)
+        for index, elemento in enumerate(tareas):
+            print(f"{index + 1}: {elemento}")
+
+    elif accion_usuario == "editar":
+        numero = int(input("Dame número de tarea a cambiar: "))
+        numero = numero - 1
+        nueva_tarea = input("Dame nueva tarea: ")
+        tareas[numero] = nueva_tarea.capitalize()
+
+    elif accion_usuario == "quitar":
+        numero = int(input("Dame número de tarea a eliminar: "))
+        numero = numero - 1
+        tareas.pop(numero)
+
     elif accion_usuario == "salir":
         break
+
     else:
-        print("Hey, ingresa una opcion valida")
+        print("Hey, has ingresado una instrucción desconocida")
 
 print("¡Adios!")
+
