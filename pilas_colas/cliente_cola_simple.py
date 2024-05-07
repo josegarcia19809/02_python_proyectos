fb061bfa8bc53a74488dde53498445ace5c4cb0e
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-06 18:25:50 -0600

    Uso de Colas. Versión 1

---
+++ pilas_colas/cliente_cola_simple.py
@@ -0,0 +1,13 @@
+from cola_simple import *
+
+queue = Queue(10)
+for person in ['Don', 'Ken', 'Ivan', 'Raj', 'Amir', 'Adi']:
+    queue.insert(person)
+print('After inserting', len(queue), 'persons on the queue, it contains: \n', queue)
+print('Is queue full?', queue.isFull())
+print('Removing items from the queue:')
+
+while not queue.isEmpty():
+    print(queue.remove(), end=' ')
+
+print()
