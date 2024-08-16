498c317d2235d582b837585c1e7906d8067ab6ef
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-15 21:38:20 -0600

    Predicción de ventas

---
+++ 01_fundamentos/prediccion_ventas.py
@@ -0,0 +1,9 @@
+# Programa elaborado por José Luis García 14 de agosto 2024
+# Este programa calculará las ventas generadas por la costa este
+
+print("VENTAS DE LA COSTA ESTE")
+
+ventas_totales = 86000
+ventas_este = ventas_totales * 0.58
+
+print(f"La venta de la costa este fue de {ventas_este}")
