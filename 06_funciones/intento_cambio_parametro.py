f4b799638f84b594cfa1a922adadc0bf51ee86ee
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-14 20:20:16 -0600

    Funciones: intentando cambiar un parámetro

--- 06_funciones/intento_cambio_parametro.py
+++ 06_funciones/intento_cambio_parametro.py
@@ -1,4 +1,5 @@
-# Este programa demuestra quá pasa cuando cambias el valor de un parámetro
+# intento_cambio_parametro.py
+# Este programa demuestra qué pasa cuando cambias el valor de un parámetro
 
 def cambiar_parametro(argumento):
     print("Estoy cambiando el valor")
