ba858b3ea3b1f1f578c3672a3618f94eced0fdce
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-14 20:19:49 -0600

    Funciones: alcance de una variable

---
+++ 06_funciones/alcance_variable.py
@@ -0,0 +1,11 @@
+# alcance_variable.py
+
+def mostrar_flores():
+    flores = 5
+    print(f"Tengo {flores} flores")
+    
+def main():
+    mostrar_flores()
+    # print(f"Él tiene {flores} flores")
+    
+main()
