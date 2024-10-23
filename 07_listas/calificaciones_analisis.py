a687302a553ce7f3092b90d085f2eb142c04bf72
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-23 10:43:48 -0600

    Listas: Análisis de calificaciones

---
+++ 07_listas/calificaciones_analisis.py
@@ -0,0 +1,42 @@
+# calificaciones_analisis.py
+
+
+def imprimir_linea():
+    print("-" * 20)
+
+
+# Pedir el número de asignaturas
+num_asignaturas = int(input("Dame el número de asignaturas: "))
+
+# Crear las 2 listas
+asignaturas = [""] * num_asignaturas
+calificaciones = [0] * num_asignaturas
+
+# Pedir los datos
+for index in range(num_asignaturas):
+    asignaturas[index] = input(f"Asignatura {index + 1}: ")
+    calificaciones[index] = int(input("Calificación? "))
+
+print(asignaturas)
+print(calificaciones)
+imprimir_linea()
+
+print("Materias no aprobadas")
+for index in range(num_asignaturas):
+    if calificaciones[index] < 70:
+        print(f"{asignaturas[index]}: {calificaciones[index]}")
+
+print("Materias aprobadas")
+for index in range(num_asignaturas):
+    if calificaciones[index] >= 70:
+        print(f"{asignaturas[index]}: {calificaciones[index]}")
+
+total = 0
+for index in range(num_asignaturas):
+    total = total + calificaciones[index]
+
+if num_asignaturas > 0:
+    promedio = total / num_asignaturas
+    print(f"Promedio: {promedio}")
+
+imprimir_linea()
