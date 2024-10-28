d98cec63c28e9ccd47ca68b1d390b6569a750efe
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-28 10:58:17 -0600

    Listas: máximos y mínimos

---
+++ 07_listas/list_max_min.py
@@ -0,0 +1,14 @@
+# list_max_min.py
+
+def imprimir_linea():
+    print("-" * 20)
+
+
+# Este programa mostrará los valores máximo y mínimo de una colección
+imprimir_linea()
+
+calificaciones = [89, 70, 56, 45, 12, 100]
+print("Lista: ", calificaciones)
+print("Máxima:", max(calificaciones))
+print("Mínima:", min(calificaciones))
+imprimir_linea()
