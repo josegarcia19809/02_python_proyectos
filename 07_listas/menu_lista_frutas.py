8717a96794899e45644766538e6a8669d90c85e7
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-28 11:00:39 -0600

    Listas: usando un menú. Inicio de proyecto

---
+++ 07_listas/menu_lista_frutas.py
@@ -0,0 +1,14 @@
+# menu_lista_frutas.py
+
+frutas = ["manzana", "pera", "piña", "naranja", "lima"]
+
+
+def menu():
+    print("-" * 30)
+    print("Uso de una lista de frutas")
+    print("1. Añadir una fruta")
+    print("2. Mostrar lista frutas")
+    print("3. Buscar una fruta")
+    print("4. Borrar una fruta")
+    print("5. Cambiar una fruta")
+    print("6. Salir")
