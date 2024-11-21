6f4e898fc5ad58e051abc805c34e4fb28b54f6a4
josegarcia19809 <josegarcia19809@gmail.com>
2024-11-20 21:26:22 -0600

    18 Turtle: Understanding Turtle Graphics and How to use the Documentation

---
+++ 18_turtle/ventana.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+mi_tortuga = Turtle()
+mi_tortuga.shape('turtle')
+mi_tortuga.color('red')
+mi_tortuga.forward(100)
+mi_tortuga.right(90)
+
+screen = Screen()
+screen.exitonclick()
